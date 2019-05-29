f210884b0d13e839b41d43de8c00480372d42503
Vítor Borba Rodrigues <vitorborbarodrigues@gmail.com>
2019-05-29 13:59:58 -0300

    Comments
    Add some comments describing the process

--- Concrete Crack Classification Data Prep.py
+++ Concrete Crack Classification Data Prep.py
@@ -15,7 +15,8 @@ def create_training_data():
     for category in categories:
         path = os.path.join(data_directory, category)
         class_num = categories.index(category)
-
+        
+        # read and resize the images and append to training_data a list with the image itself and its class number
         for img in os.listdir(path):
             img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
             new_array = cv2.resize(img_array, (img_size, img_size))
@@ -32,12 +33,14 @@ print("Training data successfully shuffled!!")
 X_data = []
 y = []
 
+# create X with the features (the images) and y with the targets (labels)
 for features, label in training_data:
     X_data.append(features)
     y.append(label)
 
 print("X and y data successfully created!!")
 
+# reshape the image to be on the correct format for tensorflow (nº images, width, height, channels)
 print("Reshaping X data...")
 X = np.array(X_data).reshape(len(X_data), img_size, img_size, 1)
 print("X data successfully reshaped!!")
